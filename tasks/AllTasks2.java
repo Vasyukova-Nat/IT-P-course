062a8200a0724493e0af4c6e37d60cc629c36ba4
Васюкова Наталья Павловна <144936520+Vasyukova-Nat@users.noreply.github.com>
2024-09-29 14:30:34 +0300

    +Tasks 2/6

--- tasks/AllTasks2.java
+++ tasks/AllTasks2.java
@@ -19,11 +19,37 @@ public class AllTasks2 {
 
         System.out.println("\n" + "Task 4");
         double[] m4 = { 3.5, 7.0, 1.5, 9.0, 5.5 };
-        System.out.println("Ответ: " + Arrays.toString(normalizator(m4)));
+        System.out.println("Ответ: " + Arrays.toString(normalizatorinator(m4)));
 
         System.out.println("\n" + "Task 5");
         double[] m5 = { 1.6, 0, 212.3, 34.8, 0, 27.5 };
-        System.out.println("Ответ: " + compressedNums(m5));
+        System.out.println("Ответ: " + compressedNums_inator(m5));
+
+        System.out.println("\n" + "Task 6");
+        String str = "helloWorld";
+        System.out.println("Для " + str + " ответ: " + camelToSnake_inator(str));
+
+        System.out.println("\n" + "Task 7");
+        int[] m6 = { 3, 5, 8, 1, 2, 4 };
+        System.out.println("Для " + Arrays.toString(m6) + " ответ: " + secondBiggest_inator(m6));
+
+        System.out.println("\n" + "Task 8");
+        // String str6 = "baobab";
+        String str6 = "Hello, I'm under the water, please help me"; // String str6 =
+        // "baeobimnbzedbv";
+        String markerSymbol = "e";
+        System.out.println("Для " + str6 + " ответ: \n" + localReverse_inator(str6, markerSymbol));
+
+        System.out.println("\n" + "Task 9");
+        int a8 = 1;
+        int b8 = 1;
+        int c8 = 0;
+        System.out.println("Для " + a8 + " " + b8 + " " + c8 + " ответ: " + equalinator(a8, b8, c8));
+
+        System.out.println("\n" + "Task 10");
+        String a10 = "Eleven plus two?";
+        String b10 = "Twelve plus one!";
+        System.out.println("Для '" + a10 + "' и '" + b10 + "' ответ: " + isAnagramInator(a10, b10));
     }
 
     public static String duplicateChars(String s1, String s2) {
@@ -67,7 +93,7 @@ public class AllTasks2 {
         return (N.toUpperCase() + "." + P.toUpperCase() + ". " + S);
     }
 
-    public static double[] normalizator(double[] m) {
+    public static double[] normalizatorinator(double[] m) {
         double[] o = new double[m.length];
 
         double maxn = Arrays.stream(m).max().orElse(Double.NaN);
@@ -80,7 +106,7 @@ public class AllTasks2 {
         return o;
     }
 
-    public static String compressedNums(double[] m) {
+    public static String compressedNums_inator(double[] m) {
         Arrays.sort(m);
         System.out.println("Дано: " + Arrays.toString(m));
 
@@ -98,5 +124,73 @@ public class AllTasks2 {
         // System.out.println(Arrays.toString(o));
 
         return Arrays.toString(o);
+    }
+
+    public static String camelToSnake_inator(String s) {
+        String o = "";
+        for (int i = 0; i < s.length(); i++) {
+            String symbol = "" + s.charAt(i); // тут преобразуем в строку длиной 1. Либо можно
+                                              // Character.toUpperCase(s.charAt(i)); - класс Character
+            if (symbol == symbol.toUpperCase()) {
+                o += "_" + symbol.toLowerCase();
+            } else {
+                o += symbol;
+            }
+        }
+        return o;
+    }
+
+    public static int secondBiggest_inator(int[] m6) {
+        int[] copy = m6.clone();
+        Arrays.sort(copy);
+        return copy[copy.length - 2];
+    }
+
+    public static String localReverse_inator(String s, String marker) {
+        String o = "";
+        int markerCount = 0;
+        String temp = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != marker.charAt(0)) {
+                if ((markerCount != 1)) {
+                    temp += "" + s.charAt(i);
+                } else {
+                    temp = "" + s.charAt(i) + temp;
+                }
+            } else {
+                o += temp + marker;
+                temp = "";
+                if (markerCount == 2) {
+                    markerCount = 1;
+                } else {
+                    markerCount += 1;
+                }
+            }
+        }
+        o += temp;
+        return o;
+    }
+
+    public static int equalinator(int a, int b, int c) {
+        int o = 0;
+        if (a == b && b == c) {
+            o = 3;
+        } else if (a == b || b == c || a == c) {
+            o = 2;
+        }
+        return o;
+    }
+
+    public static Boolean isAnagramInator(String a, String b) {
+        String alphabet = "[^a-z]";
+        char[] arrayA = a.toLowerCase().replaceAll(alphabet, "").toCharArray(); // .toLowerCase должен быть раньше
+                                                                                // .replaceAll(alphabet, иначе маска
+                                                                                // пропускает заглавные символы
+        Arrays.sort(arrayA);
+        char[] arrayB = b.toLowerCase().replaceAll(alphabet, "").toCharArray();
+        Arrays.sort(arrayB);
+        // System.out.println(Arrays.toString(arrayA) + " " + Arrays.toString(arrayB));
+        return Arrays.equals(arrayA, arrayB);
+
     }
 }
