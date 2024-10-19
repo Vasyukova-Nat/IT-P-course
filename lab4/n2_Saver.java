390d173c8dfbef18e67428baa00ceecf10e5afa2
Васюкова Наталья Павловна <144936520+Vasyukova-Nat@users.noreply.github.com>
2024-10-19 19:05:24 +0300

    +n2_Saver

--- lab4/n2_Saver.java
+++ lab4/n2_Saver.java
@@ -9,24 +9,33 @@ import java.io.IOException;
 
 public class n2_Saver {
     public static void main(String[] args) {
-        // File file = new File("n2_meow.txt");
-
         try {
             // Файл для чтения
             FileReader fileReader = new FileReader("C:\\Users\\lucky\\Desktop\\ITIP\\lab4\\n2_meow.txt");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line;
 
+            // Файл для сохранения
+            FileWriter fileWriter = new FileWriter("C:\\Users\\lucky\\Desktop\\ITIP\\lab4\\new_file.txt");
+            // FileWriter fileWriter = new FileWriter("C:\\Users\\lucky\\Desktop\\Защищенная
+            // директория\\new.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            // Цикл перебора и сохранения строк файла
             while ((line = bufferedReader.readLine()) != null) { // Используется метод readLine() для чтения построчно.
                                                                  // Если больше строк нет, возвращается null
-                System.out.println(line);
+                bufferedWriter.write(line + "\n");
             }
+            bufferedWriter.flush();
+            bufferedWriter.close();
             bufferedReader.close();
         } catch (FileNotFoundException ex) {
-            System.err.println("Файл для чтения не найден.");
+            System.err.println("Файл не найден / Отказано в доступе");
+            // ex.printStackTrace(); // выведет путь, пройденный программой до ошибки
+        } catch (SecurityException e) {
+            System.err.println("Ошибка доступа/безопасности");
         } catch (IOException e) {
-            System.out.println("Ошибка при чтении файла");
-            // e.printStackTrace(); // выведет путь, пройденный программой до ошибки
+            System.out.println("Ошибка при работе с файлом");
         }
     }
 }
