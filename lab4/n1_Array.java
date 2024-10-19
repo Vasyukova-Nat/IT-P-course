b331203157cbd68015a1d06088cafb918293c21a
Васюкова Наталья Павловна <144936520+Vasyukova-Nat@users.noreply.github.com>
2024-10-19 17:39:50 +0300

    (small)

--- lab4/n1_Array.java
+++ lab4/n1_Array.java
@@ -2,19 +2,23 @@ package lab4;
 
 public class n1_Array {
     public static void main(String[] args) {
-        Object[] arr = { 1, 2, 3, 4, 5 };
+        Object[] arr = { 1, 2, 3, 4, 9 };
+        // arr = null; // обнуление массива
         try {
             System.out.println(calculateAverage(arr));
         } catch (ClassCastException e) { // можно писать сразу 2 исключения: NumberFormatException | ClassCastException
             System.out.println("Ошибка. Введено не число. Измените входные данные и попробуйте еще раз.");
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Ошибка: выход за границы массива.");
+            System.out.println("Ошибка. Выход за границы массива.");
+        } catch (NullPointerException e) {
+            System.out.println("Ошибка. На вход поступил пустой массив");
         }
+
     }
 
     public static double calculateAverage(Object[] arr) {
-        int sum = 0;
-        for (int i = 0; i < arr.length + 1; i++) { // здесь может возникнуть исключение выхода за границы
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) { // здесь может возникнуть исключение выхода за границы
             sum += (int) arr[i]; // здесь может возникнуть исключение ClassCastException
         }
         return (sum / arr.length);
---
+++ lab4/n2_Saver.java
@@ -0,0 +1,5 @@
+package lab4;
+
+public class n2_Saver {
+
+}
